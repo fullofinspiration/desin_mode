05e0a4d302628570d9ca3fa7fc1d39002f91a949
zhangzhenpeng <zhenpeng0506@163.com>
2018-04-21 22:33:13 +0800

    增加注释

--- src/fullofinspiration/com/github/decorator/Decorator.java
+++ src/fullofinspiration/com/github/decorator/Decorator.java
@@ -1,5 +1,10 @@
 package fullofinspiration.com.github.decorator;
 
+/**
+ * 装饰模式，Person并不知道这个装饰类的存在，相当于Decorator对这个类进行了一个包裹（加入了一个
+ * Person类），同时又继承了Person类，这样就可以对该接口已有的实现类进行修饰（但是过程我也不是很清楚）
+ * 然后一些新增的功能
+ */
 public abstract class Decorator extends Person {
     @Override
     public void wearring() {
